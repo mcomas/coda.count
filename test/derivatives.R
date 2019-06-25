fef7b6b75ec975dad226b590fad122da559a564f
mcomas <mar.comas@udg.edu>
2019-06-25 18:53:56 +0200

    Reducing calculations

--- test/derivatives.R
+++ test/derivatives.R
@@ -8,8 +8,9 @@ Binv = t(MASS::ginv(B))
 I = 2
 h = c(2,0)
 deriv_1 = function(I, h, x, mu, inv_sigma, Binv){
-  w = sum(exp(Binv %*% h))
-  wBi = sum(Binv[,I] * exp(Binv %*% h))
+  eBh = exp(Binv %*% h)
+  w = sum(eBh)
+  wBi = sum(Binv[,I] * eBh)
   -sum((h-mu) * inv_sigma[,I]) + sum(x * Binv[,I]) - sum(x) * wBi / w
 }
 deriv_1(1, h, x, mu, inv_sigma, Binv)
@@ -17,10 +18,12 @@ coda.count::lpnm_join_deriv(0, h, mu, inv_sigma, x)
 
 J = 2
 deriv_2 = function(I, J, h, x, mu, inv_sigma, Binv){
-  w = sum(exp(Binv %*% h))
-  wBi = sum(Binv[,I] * exp(Binv %*% h))
-  wBj = sum(Binv[,J] * exp(Binv %*% h))
-  wBij = sum(Binv[,I] * Binv[,J] * exp(Binv %*% h))
+  eBh = exp(Binv %*% h)
+  w = sum(eBh)
+  #wBm = t(Binv) %*% eBh
+  wBi = sum(Binv[,I] * eBh) # wBm[I]
+  wBj = sum(Binv[,J] * eBh) # wBm[J]
+  wBij = sum(Binv[,I] * Binv[,J] * eBh)
   -inv_sigma[I,J] - sum(x) * ( -wBi * wBj/ w^2 + wBij / w)
 }
 deriv_2(1, 1, h, x, mu, inv_sigma, Binv)
