5bd8007432d629e0bd3e1b2c1007fdc378850696
mcomas <mar.comas@udg.edu>
2019-11-06 21:08:14 +0100

    Checking for Montecarlo integration

--- tests/testthat/test-lrnm_fitting.R
+++ tests/testthat/test-lrnm_fitting.R
@@ -1,5 +1,5 @@
 test_that("fitting", {
   set.seed(1)
   X = rlrnm(n = 10, size = 1000, mu = c(0,0), sigma = diag(2))
-  expect_output(fit_lrnm(X, method = 'montecarlo'))
+  expect_type(fit_lrnm(X, method = 'montecarlo'), "list")
 })
