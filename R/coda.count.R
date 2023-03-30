b61183c5c0f64bfcd1a0dc56ba9eaa3b6e5ec522
mcomas <marc.comas@gmail.com>
2023-03-30 10:03:42 +0200

    package documentation

--- R/coda.count.R
+++ R/coda.count.R
@@ -1,3 +1,5 @@
+#' @description
+#'coda.count is a package to deal with a multivariate count dataset with the goal of using the dataset for a log-ratio analysis.
 #' @useDynLib coda.count
 #' @importFrom Rcpp evalCpp
-NULL
+"_PACKAGE"
